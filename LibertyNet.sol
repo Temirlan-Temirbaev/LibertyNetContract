d26d92cfc66f5496a288c57092b719b0060a0ec2
Temirbaev Temirlan <temirlan213@bk.ru>
2023-11-21 11:20:16 +0600

    refactor

--- LibertyNet.sol
+++ LibertyNet.sol
@@ -1,100 +1,115 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-contract LibertyNet {
+
+contract SecureMessage {
     struct User {
-        string nickname;
-        string avatar;
+        address userAddress;
         bool isModerator;
-        uint256[] postIds;
         bool isBanned;
-        address[] boosters;
+        uint256 subscriptionCost;
+        address[] subscribers;
+        Subscription[] subscriptions;
+    }
+
+    struct Subscription {
+        uint256 cost;
+        uint256 startedAt;
+        address account;
     }
 
     struct Post {
+        uint256 id;
         address author;
-        string content;
-        string multiMedia;
-        uint256[] commentIds;
         bool isPublic;
     }
 
-    struct Comment {
-        address authod;
-        string content;
-    }
-
-    mapping (address => User) private users;
     address private _owner;
     Post[] private posts;
-    Comment[] private comments;
+    mapping(address => User) public users;
 
     constructor(){
         _owner = msg.sender;
     }
 
     modifier onlyOwner() {
-        require(msg.sender == _owner, "Only owner can call this function");
+        require(msg.sender == _owner, "Only owner has access");
         _;
     }
 
-    modifier onlyUnBanned() {
-        require(users[msg.sender].isBanned == false, "You are banned!");
+    modifier onlyModerator(){
+        require(users[msg.sender].isModerator == true, "Only moderators has access");
         _;
     }
 
-    modifier onlyModerator() {
-        require(users[msg.sender].isModerator == true, "You have no rights for this operation!");
-        _;
+    function changeIsModerator(address user) public onlyOwner {
+        users[user].isModerator = !users[user].isModerator;
     }
 
-    function changeUserData(string memory nickname, string memory avatar) public {
-        require(bytes(nickname).length > 0, "Nickname cannot be empty");
-        require(bytes(avatar).length > 0, "Avatar cannot be empty");
-        users[msg.sender] = User({
-            nickname: nickname,
-            avatar: avatar,
-            isModerator: users[msg.sender].isModerator,
-            postIds: users[msg.sender].postIds,
-            isBanned: users[msg.sender].isBanned,
-            boosters: users[msg.sender].boosters
+    function changeBan(address user) public onlyModerator {
+        users[user].isBanned = !users[user].isBanned;
+    }
+
+    function buySubscription(address payable user) public {
+        address payable recipient = payable(users[user].userAddress);
+        recipient.transfer(users[user].subscriptionCost);
+        Subscription memory newSubscription = Subscription({
+            cost: users[user].subscriptionCost,
+            startedAt: block.timestamp,
+            account: msg.sender
         });
+
+        users[user].subscriptions.push(newSubscription);
     }
 
-    function switchIsBanned(address user) public onlyModerator {
-        if (users[user].isBanned == true) users[user].isBanned = false;
-        else users[user].isBanned = false;
-    }
-
-    function switchUserRole(address userAddress, bool newRole) public onlyOwner {
-        require(users[userAddress].isBanned == false, "User is banned");
-        users[userAddress].isModerator = newRole;
-    }
-
-    function createPost(string memory content, string memory multiMedia, bool isPublic) public onlyUnBanned {
-        uint256 postId = posts.length;
+    function post(uint256 id, bool isPublic) public {
         posts.push(Post({
-            author: msg.sender,
-            content: content,
-            multiMedia: multiMedia,
-            commentIds: new uint256[](0),
-            isPublic: isPublic
+            id: id,
+            isPublic: isPublic,
+            author: msg.sender
         }));
-
-        // Update user's postIds
-        users[msg.sender].postIds.push(postId);
     }
 
-    function comment(uint256 postId, string memory content) public onlyUnBanned {
-        require(postId < posts.length, "Invalid post ID");
+    function getPost(uint256 id) public view returns (Post memory) {
+        bool isFound = false;
+        for (uint256 i = 0; i < posts.length; i++) {
+            if (posts[i].id == id) {
+                isFound = true;
+                require(users[posts[i].author].isBanned == false, "This post has been banned");
+                if (posts[i].isPublic == false) {
+                    bool isHasSubscription = false;
+                    for (uint256 j = 0; j < users[msg.sender].subscriptions.length; j++) {
+                        if (users[msg.sender].subscriptions[j].account == posts[i].author) {
+                            isHasSubscription = true;
+                            if (users[msg.sender].subscriptions[j].startedAt - block.timestamp > 0) {
+                                revert("Your subscription is expired!");
+                            }
+                        }
+                    }
+                    require(isHasSubscription == true, "You have not subscription");
+                    return posts[i];
+                }
+                return posts[i];
+            }
+        }
+        require(isFound == true, "Not found");
+    }
 
-        uint256 commentId = comments.length;
-        comments.push(Comment({
-            authod: msg.sender,
-            content: content
-        }));
+    function getAuthorsPost(address user) public view returns (Post[] memory) {
+        Post[] memory usersPosts = new Post[](posts.length);
+        uint256 count = 0;
+        for (uint256 i = 0; i < posts.length; i++) {
+            Post memory post = getPost(posts[i].id);
+            if (post.author == user) {
+                usersPosts[count] = post;
+                count++;
+            }
+        }
 
-        // Update post's commentIds
-        posts[postId].commentIds.push(commentId);
+        Post[] memory result = new Post[](count);
+        for (uint256 i = 0; i < count; i++) {
+            result[i] = usersPosts[i];
+        }
+        return result;
     }
 
 }
