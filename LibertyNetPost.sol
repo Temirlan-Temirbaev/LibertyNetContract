61d23698fae89a2f40b44674ecd04a7ee1542ae4
Temirbaev Temirlan <temirlan213@bk.ru>
2023-12-03 18:27:16 +0600

    feat/post

--- LibertyNetPost.sol
+++ LibertyNetPost.sol
@@ -2,62 +2,21 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "./LibertyNetModeration.sol";
 
-contract PostNFT is ERC721 {
+contract PostNFT is Moderation, ERC721  {
     uint256 public nextTokenId;
-    address private owner;
+    mapping(uint256 => PostData) public postData;
+
 
     struct PostData {
         string content;
         string mediaContentUrl;
         address author;
-    }
-
-    mapping(uint256 => PostData) public postData;
-
-    address[] blackList;
-    address[] moderators;
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Not the contract owner");
-        _;
-    }
-
-    modifier onlyModerator() {
-        bool isModerator = false;
-        for (uint256 i = 0; i < moderators.length; i++) {
-            if (moderators[i] == msg.sender) {
-                isModerator = true;
-            }
-        }
-        require(isModerator == true, "You have no rights to this function");
-        _;
+        uint256 postId;
     }
 
     constructor() ERC721("LibertyNetPost", "LNPNFT") {
-        owner = msg.sender;
-    }
-
-    function addToModerators(address userAddress) public onlyOwner {
-        moderators.push(userAddress);
-    }
-
-    function addToBlackList(address userAddress) public onlyModerator {
-        blackList.push(userAddress);
-    }
-
-    function mintPostNFT(
-        string memory content,
-        string memory mediaContentUrl
-    ) external {
-        for (uint256 i = 0; i < blackList.length; i++) {
-            if (blackList[i] == msg.sender) {
-                revert("You are banned");
-            }
-        }
-        uint256 tokenId = nextTokenId++;
-        _safeMint(msg.sender, tokenId);
-        postData[tokenId] = PostData(content, mediaContentUrl, msg.sender);
     }
 
     function getPostsByAuthor(address author) public view returns (PostData[] memory) {
@@ -85,29 +44,6 @@ contract PostNFT is ERC721 {
         return usersPosts;
     }
 
-    function stringContainsSubstring(string memory _string, string memory substring) internal view returns (bool) {
-        bytes memory stringBytes = bytes(_string);
-        bytes memory substringBytes = bytes(substring);
-        uint256 substringLength = substringBytes.length;
-        uint256 j;
-        for (uint256 i = 0; i < stringBytes.length; i++) {
-            if (stringBytes[i] == substringBytes[0]) {
-                j = 1;
-                while ((i + j) < stringBytes.length && j < substringLength) {
-                    if (stringBytes[i + j] != substringBytes[j]) {
-                        break;
-                    }
-                    j++;
-                }
-                if (j == substringLength) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-
     function getPostsByContent(string memory contentSubstring) public view returns (PostData[] memory) {
         uint256 count = 0;
 
@@ -133,5 +69,46 @@ contract PostNFT is ERC721 {
         return postsWithSubstring;
     }
 
+    function mintPostNFT(
+        string memory content,
+        string memory mediaContentUrl,
+        uint256 id
+    ) external isNotBanned {
+        uint256 tokenId = nextTokenId++;
+        _safeMint(msg.sender, tokenId);
 
+        postData[tokenId] = PostData(content, mediaContentUrl, msg.sender, id);
+
+    }
+
+    function edit(string memory content, string memory mediaContentUrl, uint256 tokenId) public isNotBanned{
+        require(ownerOf(tokenId) != address(0), "Token ID does not exist");
+        require(ownerOf(tokenId) == msg.sender, "You don't have permission to edit this post");
+
+        // Update post data
+        postData[tokenId].content = content;
+        postData[tokenId].mediaContentUrl = mediaContentUrl;
+    }
+
+    function stringContainsSubstring(string memory _string, string memory substring) internal view returns (bool) {
+        bytes memory stringBytes = bytes(_string);
+        bytes memory substringBytes = bytes(substring);
+        uint256 substringLength = substringBytes.length;
+        uint256 j;
+        for (uint256 i = 0; i < stringBytes.length; i++) {
+            if (stringBytes[i] == substringBytes[0]) {
+                j = 1;
+                while ((i + j) < stringBytes.length && j < substringLength) {
+                    if (stringBytes[i + j] != substringBytes[j]) {
+                        break;
+                    }
+                    j++;
+                }
+                if (j == substringLength) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
